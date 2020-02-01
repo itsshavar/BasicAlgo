1b2adf485f0ca2ea6870dac9c9bb5b136170d48a
Shashi Tripathi <shishu@Shashis-MacBook-Air.local>
2020-02-02 00:34:40 +0530

    Added Matrix Chain Multiplication

--- matrix_chain_multiplication.py
+++ matrix_chain_multiplication.py
@@ -1,26 +1,45 @@
-def print_(m):
-    for i in m:
-        print(i)
-def matrix_chain(p):
-    n = len(p)
-    m = [[0]*n]*n
-    s = [[0] * n ] * n
-    for l in range(2,n):
-        for i in range(1,n-l+1):
-            j = i+l-1
-            m[i][j] = float('inf') 
-            for k in range(i,j):
-                print(m[i][k],m[k+1][j],p[i-1]*p[k]*p[j])
-                q = m[i][k]+m[k+1][j]+p[i-1]*p[k]*p[j]
-                if q < m[i][j]:
-                    m[i][j] = q
-                    s[i][j] = k
-    return m,s
+
+"""
+Problem Statement 
+=========================
+Matrix chain multiplication (or Matrix Chain Ordering Problem, MCOP) is an optimization problem that can be solved using 
+dynamic programming. Given a sequence of matrices, the goal is to find the most efficient way to multiply these matrices.
+The problem is not actually to perform the multiplications, but merely to decide the sequence of the matrix multiplications 
+involved.
+
+There are many options because matrix multiplication is associative. In other words, no matter how the product is 
+parenthesized, the result obtained will remain the same. For example, for four matrices A, B, C, and D, we would have:
+
+((AB)C)D = (A(BC))D = (AB)(CD) = A((BC)D) = A(B(CD)).
+
+
+Runtime Analysis:
+=======================
+Time complexity - O(N^3)
+
+"""
+from typing import List
+
+def matrix_chain_order(matrices : List[int]) -> int :
+    matrices_length = len(matrices)
+
+    T = [[0 for _ in range(matrices_length)] for _ in range(matrices_length)]
+
+    for gap in range(2, matrices_length):
+        for index_i in range(0, matrices_length - gap):
+            index_j = index_i + gap
+            T[index_i][index_j] = 10000
+            for index_k in range(index_i + 1, index_j):
+                temp = T[index_i][index_k] + T[index_k][index_j] + matrices[index_i] * matrices[index_k] * matrices[index_j]
+                if temp < T[index_i][index_j]:
+                    T[index_i][index_j] = temp
+
+    return T[0][-1]
 
 
 def main():
     matrices = [4, 2, 3, 5, 3]
-    print(matrix_chain(matrices)[0])
+    print(matrix_chain_order(matrices))
 
 if __name__ == "__main__":
     main()
